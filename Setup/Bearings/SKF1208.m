3547ec0720463d0a76146ce5f55466245c7b096e
alxhslm <alexhaslam12@gmail.com>
2019-01-24 19:03:45 +0000

    Update REB parameters based on Solidworks model

--- Setup/Bearings/SKF1208.m
+++ Setup/Bearings/SKF1208.m
@@ -1,4 +1,4 @@
-function REB = SKF1208(model,alpha)
+function REB = SKF1208(model)
 
 REB.Setup.Type = 'ball';
 REB.Setup.Arrangement = 'double_inline';
@@ -6,30 +6,25 @@ REB.Setup.Z = 18*2;
 % REB.Setup.CbParallel = diag([0 0 0 0]);
 % REB.Setup.KbParallel = diag([Inf 0 Inf 0]);
 
-REB.Geometry.D  = 11.4E-3;
-REB.Geometry.dm = 60.4E-3;
-% REB.Geometry.z0 = 5.8E-3;
-% REB.Geometry.alpha0 = atan(REB.Geometry.z0*2/REB.Geometry.dm);
-if nargin < 2
-    alpha = 10.87*pi/180;
-end
-    
-REB.Geometry.alpha0 = alpha;
-REB.Geometry.z0 = REB.Geometry.dm/2*tan(REB.Geometry.alpha0);
-REB.Geometry.cr = 2.5E-6;
-REB.Geometry.cz = 0E-6;
+D = 2*4.3655E-3;
+dm = 60.146E-3;
+REB.Geometry.D  = D;
+REB.Geometry.dm = dm;
 
-REB.Geometry.do = REB.Geometry.dm + REB.Geometry.cr + REB.Geometry.D*cos(REB.Geometry.alpha0);
-REB.Geometry.di = REB.Geometry.dm - REB.Geometry.cr - REB.Geometry.D*cos(REB.Geometry.alpha0);
+Ro = 39.89311820E-3;
+Ri = D/2 + 0.0001E-3;
 
-REB.Geometry.ro = REB.Geometry.do/cos(REB.Geometry.alpha0); %0.5*REB.dm/cos(REB.alpha0)+REB.D/2;
-REB.Geometry.ri = REB.Geometry.D/2 + 1E-3;
+REB.Geometry.alpha0 = 0.5*14.58*pi/180;
+REB.Geometry.z0 = (9E-3)/2;
+REB.Geometry.cr = 0.05296261E-3;
+REB.Geometry.cz = 0;
 
-% REB.Geometry.psi0 = (pi/2 - pi/REB.Setup.Z*floor((max(REB.Setup.Z-5,0)*0.5 + 1)));
+REB.Geometry.RRaceo = Ro;
+REB.Geometry.RRacei = Ri;
 
 %steel
 REB.Material.v = 0.3;
-REB.Material.E = 200E9;
+REB.Material.E = 210E9;
 REB.Material.rho = 8000;
  
 %ISO VG32 oil
@@ -54,5 +49,8 @@ if nargin < 1
 end
 REB.Model.Name = model;
 
-% REB = setupREB(REB);
-% plot_REB(REB)
+% REB1 = setupREB(REB);
+% structdiff(REB,REB1)
+% REB2 = setupREB(REB1);
+% structdiff(REB,REB2)
+% plot_REB(REB2);
