a6d82e246322b36e204b17d83cf1bf519b6cc76d
alxhslm <alexhaslam12@gmail.com>
2019-01-25 14:28:35 +0000

    Fix for when the radial clearance is very large

--- Bearings/REB/functions/race_geometry.m
+++ Bearings/REB/functions/race_geometry.m
@@ -1,7 +1,7 @@
 function [li,lo,alpha_i,alpha_o] = race_geometry(Xz,Xr,Az,Ar)
 %compute contact angle and deflections
-li = sqrt((Az-Xz).^2  + (Ar-Xr).^2);
-lo = sqrt(Xz.^2  + Xr.^2);
+li = sqrt((Az-Xz).^2  + (Ar-Xr).^2).*sign(Ar-Xr);
+lo = sqrt(Xz.^2  + Xr.^2).*sign(Xr);
 
 alpha_i = atan2(Az-Xz,Ar-Xr);
 alpha_o = atan2(Xz,Xr);
