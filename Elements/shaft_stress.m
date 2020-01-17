98d83177b25ebc0e28f645be83b4f09e61d6a9b0
alxhslm <alexhaslam12@gmail.com>
2020-01-17 15:52:40 +0000

    Function to compute shaft bending stresses

---
+++ Elements/shaft_stress.m
@@ -0,0 +1,23 @@
+function [sigma,z] = shaft_stress(P,X)
+for i = 1:length(P.Rotor)
+   Sr = P.Rotor{i}.S;
+   for j = 1:length(P.Rotor{i}.Shaft)
+       Ss = P.Rotor{i}.Shaft{j}.S * Sr;
+       
+       for k = 1:length(P.Rotor{i}.Shaft{j}.Element)
+           Se = P.Rotor{i}.Shaft{j}.Element{k}.S*Ss;
+           Re = P.Rotor{i}.Shaft{j}.Element{k}.R;
+            
+           Fs = mtimesx(Re' * P.Rotor{i}.Shaft{j}.Element{k}.K * Re * Se,X);
+           My = Fs(3,:,:);
+           Mx = Fs(4,:,:);
+       
+           M = hypot(Mx,My);
+           for n = 1:size(M,2)
+                sigma{i}{j}(k,n,:) = M(:,n,:) * P.Rotor{i}.Shaft{j}.Section.ro/P.Rotor{i}.Shaft{j}.Section.I;
+           end
+           z{i}{j}(k) = P.Rotor{i}.Shaft{j}.Element{k}.z;
+       end
+   end
+end
+
